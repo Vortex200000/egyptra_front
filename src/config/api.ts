4747b06a15776c54f93b4b97bf97faa77edaade9
omaressam744 <omaressam744@gmail.com>
2025-09-21 10:49:28 -0700

    switch to railwlay

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 // export const API_URL = "http://127.0.0.1:8000";
 
-// export const API_URL = "https://egypt-ra.osc-fr1.scalingo.io";
+
 export const API_URL = "https://egypra-production.up.railway.app";
 
